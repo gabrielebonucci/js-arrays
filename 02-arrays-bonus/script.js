515db6fa3006b3f182ad096c9881be10751f6d6e
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-08-29 17:26:57 +0200

    svolgimento esercizi bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -38,12 +38,31 @@ console.log(longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const uptadedTeachers = [];
+
+for (let i = 0; i < teachers.length; i++) {
+
+  const currentTeacher = teachers[i];
+
+  if (currentTeacher !== 'Ed') {
+    uptadedTeachers.push(currentTeacher);
+  }
+}
+console.log(uptadedTeachers)
 
 
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent = true;
+  }
+}
+
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
