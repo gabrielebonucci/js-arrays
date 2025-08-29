1714b8d6d42ed61ec128d0e6d73223480ad0380f
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-08-29 17:43:43 +0200

    esercitazione conclusa

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -65,4 +65,18 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+
+  teachersString = teachersString + teachers[i];
+
+  if (i < teachers.length - 1) {
+    teachersString = teachersString + ", ";
+  }
+
+}
+console.log(teachersString);
+
+
+  
