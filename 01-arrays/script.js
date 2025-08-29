30ac52070f549afb42e26fe6357474d138a5f661
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-08-29 15:50:18 +0200

    Esercizio completato

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -38,11 +38,19 @@ teachers.push("Vanessa");
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
+const isTeachersEmpty = teachers.length === 0;
+
+console.log(isTeachersEmpty);
+
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
